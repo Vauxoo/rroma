04a451ebd509ea61ed7944c9f5944a2445d2d500
Alejandro Santillan <asantillan@vauxoo.com>
2021-05-10 18:00:05 +0000

    [DUMMY] Test 2 l10n_cr_edi_deactivate_sh

--- l10n_cr_edi_deactivate_sh/models/account_move.py
+++ l10n_cr_edi_deactivate_sh/models/account_move.py
@@ -10,6 +10,14 @@ class AccountMove(models.Model):
     _inherit = 'account.move'    
 
     def action_post(self):
-        if os.environ.get('environment') in ['staging', 'dev'] and not self.env.company.sudo().l10n_cr_edi_test_env:
+        if self._get_odoo_sh_environment() in ['staging', 'dev'] and not self.env.company.sudo().l10n_cr_edi_test_env:
             self.env.company.sudo().l10n_cr_edi_test_env = True
         return super().action_post()
+
+    @api.model
+    def _get_odoo_sh_environment(self)
+        """Get the Odoo Sh environment type
+        :return: The instance environment type. production | staging | dev
+        :rtype: string
+        """
+        return os.environ.get('ODOO_STAGE', False)
